4f23590225b8977f9329463b94cb6f08742a4e72
Jascha Ehrenreich <jascha@jaeh.at>
2016-02-15 19:22:16 +0100

    crude fontencode poc

---
+++ bin/fontencode.js
@@ -0,0 +1,40 @@
+var get = require('http').get;
+
+// Get binary file using XMLHttpRequest
+function getBinary(file) {
+  return get(file);
+}
+
+// Base64 encode binary string
+// Stolen from http://stackoverflow.com/questions/7370943/retrieving-binary-file-content-using-javascript-base64-encode-it-and-reverse-de
+function base64Encode(str) {
+  var CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  var out = "", i = 0, len = str.length, c1, c2, c3;
+  while (i < len) {
+    c1 = str.charCodeAt(i++) & 0xff;
+    if (i == len) {
+      out += CHARS.charAt(c1 >> 2);
+      out += CHARS.charAt((c1 & 0x3) << 4);
+      out += "==";
+      break;
+    }
+    c2 = str.charCodeAt(i++);
+    if (i == len) {
+      out += CHARS.charAt(c1 >> 2);
+      out += CHARS.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+      out += CHARS.charAt((c2 & 0xF) << 2);
+      out += "=";
+      break;
+    }
+    c3 = str.charCodeAt(i++);
+    out += CHARS.charAt(c1 >> 2);
+    out += CHARS.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+    out += CHARS.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
+    out += CHARS.charAt(c3 & 0x3F);
+  }
+  return out;
+}
+
+var base64EncodedFont = base64Encode(getBinary("http://localhost:3000/ms-ico.woff"));
+var fontCode = "src: url('data:application/font-woff;base64," + base64EncodedFont + "') format('woff');";
+console.log(fontCode);
