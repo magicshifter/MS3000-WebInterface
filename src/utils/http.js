e40e11837c1fec1f7bd66442d107a1764d250432
Jascha Ehrenreich <jascha@jaeh.at>
2016-02-26 23:01:21 +0100

    add logging of requests, add 10 sec timeout

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,30 +1,35 @@
+const log =
+  startTime =>
+    console.log('request took', (new Date().getTime() - startTime) / 1000, 'seconds');
+
 export const fetch =
   (url, method = 'GET') =>
     new Promise((resolve, reject) => {
+      const requestStart = new Date().getTime();
+      console.log('fetching', url, 'method', method);
       const req = new window.XMLHttpRequest();
 
-      req.timeout = 3000;
+      req.timeout = 10000;
       req.open(method, url);
 
       req.onload =
         () =>
+          log(requestStart) ||
           req.status === 200
           ? resolve(req)
           : reject(new Error(req.statusText));
 
       req.onerror =
         () =>
+          log(requestStart) ||
           reject(new Error('Network Error'));
 
       req.ontimeout =
         () =>
+          log(requestStart) ||
           reject(new Error('Request timed out'));
 
-      try {
-        req.send();
-      } catch (e) {
-        console.log('error', { e });
-      }
+      req.send();
     });
 
 export const parseJSONResult =
