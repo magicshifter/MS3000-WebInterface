9674133c9360f8c575e3553550f719e33453e9aa
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-04 13:40:29 +0100

    update http utils

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,33 +1,55 @@
 const log =
   startTime =>
-    console.log('request took', (new Date().getTime() - startTime) / 1000, 'seconds');
+    console.log(
+      'request took',
+      (new Date().getTime() - startTime) / 1000,
+      'seconds'
+    );
+
+export const fetchJSON =
+  ({ url, method = 'GET' }) =>
+    fetch({
+      url,
+      method,
+      json: true,
+    });
 
 export const fetch =
-  ({ url, method = 'GET' }) =>
+  ({ url, method = 'GET', json = false }) =>
     new Promise((resolve, reject) => {
       const requestStart = new Date().getTime();
-      console.log('fetching', url, 'method', method);
+      console.log('fetching', { url, method, json });
       const req = new window.XMLHttpRequest();
 
       req.timeout = 10000;
       req.open(method, url);
 
       req.onload =
-        () =>
-          log(requestStart) ||
-          req.status === 200
-          ? resolve(req)
-          : reject(new Error(req.statusText));
+        () => {
+          log(requestStart);
+          if (req.status === 200) {
+            if (json) {
+              resolve(parseJSONResult(req));
+            } else {
+              resolve(req);
+            }
+            return;
+          }
+
+          reject(new Error(req.statusText));
+        };
 
       req.onerror =
-        () =>
-          log(requestStart) ||
+        () => {
+          log(requestStart);
           reject(new Error('Network Error'));
+        };
 
       req.ontimeout =
-        () =>
-          log(requestStart) ||
+        () => {
+          log(requestStart);
           reject(new Error('Request timed out'));
+        };
 
       req.send();
     });
@@ -35,11 +57,10 @@ export const fetch =
 export const parseJSONResult =
   res => {
     let parsed = new Error('invalid Response');
+    console.log('parseJSON', { res });
     try {
-      console.log(res);
-      parsed = JSON.parse(res);
+      parsed = JSON.parse(res.response || res.responseText);
     } catch (e) {
-      console.log('json parsing error');
       parsed = e;
     }
 
