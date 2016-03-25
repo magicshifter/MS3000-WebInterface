787e0dfad9b904f25e2fca5b6a9f6eb41ca4a39e
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-25 23:06:40 +0100

    add es6-promise polyfill

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import routes from './routes';
 import Root from './containers/Root';
 import configureStore from './redux/configureStore';
 
+import es6promise from 'es6-promise';
+es6promise.polyfill();
+
 const store = configureStore(window.__INITIAL_STATE__);
 
 // Render the React application to the DOM
