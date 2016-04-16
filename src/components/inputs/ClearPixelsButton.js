c7aafdd85de7eed5a06774f92d9c57d33817b730
Jascha Ehrenreich <jascha@jaeh.at>
2016-04-16 16:37:54 +0200

    add ClearPixelsButton input

---
+++ src/components/inputs/ClearPixelsButton.js
@@ -0,0 +1,45 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+import { getIconCssClass } from 'utils/icons';
+import { pixelsType } from 'utils/propTypes';
+
+import { actions } from 'redux/modules/pixels';
+
+import classes from './ClearPixelsButton.scss';
+
+const mapStateToProps =
+  ({ pixels }) => {
+    return {
+      pixels: pixels.toJS(),
+    };
+  };
+
+export class ClearPixelsButton extends Component {
+  static propTypes = {
+    clearPixels: PropTypes.func.isRequired,
+    pixels: pixelsType.isRequired,
+  };
+
+  onClick =
+    () => {
+      const { pixels, clearPixels } = this.props;
+      clearPixels(pixels);
+    };
+
+  render =
+    () =>
+      <div className={classes['container']}>
+        <ul>
+          <li>
+            <i
+              className={getIconCssClass('eraser')}
+              onClick={this.onClick}
+            />
+          </li>
+        </ul>
+      </div>;
+}
+
+export default connect(mapStateToProps, actions)(ClearPixelsButton);
+
