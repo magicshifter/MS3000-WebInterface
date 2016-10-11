e8acc6c1a9b8b39c1d9539a59e82bcd1763125a4
jascha <jascha@jaeh.at>
2016-10-11 18:52:31 +0200

    do not copy app.css to dist/bundled

--- cli.sh
+++ cli.sh
@@ -116,7 +116,7 @@ function inline() {
   cp $JS_DIST_DIR/index.js $JS_DIST_DIR/bundled/index.js
   cp $JS_DIST_DIR/magicshifter.appcache $JS_DIST_DIR/bundled/magicshifter.appcache
   cp $JS_DIST_DIR/favicon.ico $JS_DIST_DIR/bundled/favicon.ico
-  cp $JS_DIST_DIR/app.css $JS_DIST_DIR/bundled/app.css
+  # cp $JS_DIST_DIR/app.css $JS_DIST_DIR/bundled/app.css
 
   gzip --keep --force --best dist/bundled/index.js
 
