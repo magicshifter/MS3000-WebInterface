cef7466af9576358c2f405e5048099057ac9045d
Jay Vaughan <seclorum@icloud.com>
2018-08-23 18:56:42 +0200

    s/MS3000GlobalPBuffer/MS3KG/ .. its shorter and produces more palatable constans \(.pb.h\)

--- ms3000-app/src/utils/protoBufLoader.js
+++ ms3000-app/src/utils/protoBufLoader.js
@@ -15,7 +15,7 @@ export function getProtocolBuffersPromise() {
       }
       else {
         // Obtain a message type
-        xxx.MS3KG = root.lookupType("MS3000GlobalPBuffer");
+        xxx.MS3KG = root.lookupType("MS3KG");
         xxx.root = root
 
         console.log("stage2", xxx)
