f81cde7b5054e513fbcf05992d4e8f83d7657a45
Jay Vaughan <seclorum@icloud.com>
2018-12-23 21:55:10 +0100

    fat-ms-button for web interface

--- ms3000-app/src/components/AutoInterface/EnumControl.js
+++ ms3000-app/src/components/AutoInterface/EnumControl.js
@@ -59,6 +59,11 @@ export default class EnumControl extends Component {
             <div className="pure-u-1-1 pure-u-sm-1-2 pure-u-md-1-4 pure-u-lg-1-8 ms3000-input-radio">
               <label key={f} htmlFor={genid} >
               {k} <br/>
+
+            <div class="fat-ms-button">
+            <div></div>
+            </div>
+
                 <span className="ms3000-input-flow">
                   <input
                     type="radio"
